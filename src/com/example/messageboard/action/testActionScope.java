06487b58e17de6134b106822eb1e74fa25d3a561
Javabean <448267696@qq.com>
2015-04-20 17:45:23 +0800

    test完毕，发现spring+struts2的Action的默认作用域是单例的，并不是网上说的prototype 必须加上prototype才是原型

--- src/com/example/messageboard/action/testActionScope.java
+++ src/com/example/messageboard/action/testActionScope.java
@@ -7,8 +7,8 @@ public class testActionScope {
 	private static Integer num_s = 0;
 	private Integer num_n = 0;
 
-	public int execute() {
+	public String execute() {
 		System.out.println("num_s:" + (num_s++) + "   num_n:" + (num_n++));
-		return 0;
+		return null;
 	}
 }
